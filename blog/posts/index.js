efc1ba4db8192fac0ebe1818d7530b41dc7a84d1
jdidi94 <jdididaoud404@gmail.com>
2022-06-12 12:19:17 +0200

    set deployment configuration

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -17,7 +17,7 @@ app.post("/posts", async (req, res) => {
     id: id,
     title: title,
   };
-  await axios.post("http://localhost:4005/events", {
+  await axios.post("http://event-bus-srv:4005/events", {
     type: "PostCreated",
     data: { id, title },
   });
@@ -30,5 +30,6 @@ app.post("/events", (req, res) => {
 });
 
 app.listen(4000, () => {
+  console.log("V55");
   console.log("listening on port 4000");
 });
